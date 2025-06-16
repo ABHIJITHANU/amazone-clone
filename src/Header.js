c2f1764ed7f415e37e04c1592504f0c1fcb6ed51
ABHIJITHANU/amazone-clone <abhijithsr.personal@gmail.com>
2025-06-16 20:25:22 +0530

    Header and logo base setup

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import "./Header.css";
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+  return (
+    <div className="header">
+      <img src="/assets/amazone-header-logo.png" alt="Amazon Logo" className="header_logo" />
+      <div className="header_search">
+        <input type="text" className="header_searchInput" />
+        <button className="header_searchButton">
+          <SearchIcon />
+        </button>
+        <div className="header_nav">
+          <div className="header_option">
+            <span className="header_optionLineOne">Hello Guest</span>
+            <span className="header_optionLineTwo">Sign In</span>
+          </div>
+          <div className="header_option">
+            <span className="header_optionLineOne">Returns</span>
+            <span className="header_optionLineTwo">& Orders</span>
+          </div>
+          <div className="header_option">
+            <span className="header_optionLineOne">Your</span>
+            <span className="header_optionLineTwo">Prime</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB3QZmOCDXmbA3diDkBeDGhP3Ftvwc07Mo",
+  authDomain: "e-clone-5b634.firebaseapp.com",
+  projectId: "e-clone-5b634",
+  storageBucket: "e-clone-5b634.firebasestorage.app",
+  messagingSenderId: "904618084193",
+  appId: "1:904618084193:web:221d68b1696624f5ade720",
+  measurementId: "G-B6KSSZHPDH"
+};
